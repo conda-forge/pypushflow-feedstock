25172c7e7778ca679e48770a2fa85bac8507fc44
woutdenolf <woutdenolf@users.sf.net>
2022-12-09 15:29:17 +0100

    generate

--- recipe/generate.sh
+++ recipe/generate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-grayskull pypi pypushflow --extras-require-all --extras-require-split --extras-require-test test --extras-require-exclude dev --strict-conda-forge
+grayskull pypi pypushflow --extras-require-all \
+    --extras-require-split \
+    --extras-require-test test \
+    --extras-require-exclude dev doc\
+    --strict-conda-forge
 mv pypushflow/meta.yaml .
 rm -rf pypushflow
